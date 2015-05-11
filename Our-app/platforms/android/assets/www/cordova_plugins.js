5e8b741a2cbcb9d94f6fc054849068d0d9019e41
MIAGIT <nick_coevering@hotmail.com>
2015-05-11 12:36:07 +0200

    work

---
+++ Our-app/platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,25 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
+        "id": "cordova-plugin-whitelist.whitelist",
+        "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-vibration/www/vibration.js",
+        "id": "cordova-plugin-vibration.notification",
+        "merges": [
+            "navigator.notification",
+            "navigator"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.0.1-dev",
+    "cordova-plugin-vibration": "1.0.0",
+    "cordova-plugin-geolocation": "1.0.0"
+}
+// BOTTOM OF METADATA
+});
